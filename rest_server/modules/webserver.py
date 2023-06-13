87f920b41b6d8a12047b04b374daf5e174b9b600
Flappeh <86529602+Flappeh@users.noreply.github.com>
2023-06-13 22:04:55 +0700

    Updated webserver.py

--- rest_server/modules/webserver.py
+++ rest_server/modules/webserver.py
@@ -1,108 +1,171 @@
 from flask import Flask, json, request
-import httpx
+from flask_restful import Resource, Api, reqparse, abort
+
 import requests
-import asyncio
+import os
 import json
-import sys
-companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
+from dotenv import load_dotenv
 
-# app = Flask(__name__)
+load_dotenv()
 
+app_key = os.getenv("APP_KEY")
+client_key = os.getenv("CLIENT_KEY")
+    
+header_app= {
+    "Authorization": f'Bearer {app_key}',
+    "Content-type": 'application/json',
+    "Accept": 'application/json',
+}
+header_client= {
+    "Authorization": f'Bearer{client_key}',
+    "Content-type": 'application/json',
+    "Accept": 'application/json',
+}
+
+null = None
 server_details=[
         {
-            "id":15,
+            "id":1,
             "body":{
-                "startup": "java -Xmx{{SERVER_MEMORY}}M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs/ -Daikars.new.flags=true --add-modules=jdk.incubator.vector -jar {{SERVER_JARFILE}}",
+                "startup": "java -Xmx$(({{SERVER_MEMORY}}-1024))M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs/ -Daikars.new.flags=true --add-modules=jdk.incubator.vector -jar {{SERVER_JARFILE}}",
                 "environment": {
+                    "PROJECT":"paper",
                     "SERVER_JARFILE": "paper.jar",
                     "MINECRAFT_VERSION": "latest",
-                    "BUILD_NUMBER":"latest"
-                },
-                "egg": 15,
-                "image": "ghcr.io/pterodactyl/yolks:java_18",
-                "skip_scripts": False
-            }
-        },
-        {
-            "id":16,
-            "body":{
-                "startup": "java  -Xmx{{SERVER_MEMORY}}M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs/ -Daikars.new.flags=true --add-modules=jdk.incubator.vector -jar {{SERVER_JARFILE}}",
-                "environment": {
-                    "SERVER_JARFILE": "fabric.jar",
-                    "MC_VERSION": "latest",
+                    "BUILD_NUMBER":"latest",
+                    "DL_PATH": null,
                     "FABRIC_VERSION":"latest",
-                    "LOADER_VERSION":"latest"
+                    "LOADER_VERSION":"latest",
+                    "BUILD_TYPE":"recommended",
+                    "FORGE_VERSION":null,
+                    "BUNGEE_VERSION":"latest"
                 },
-                "egg": 16,
+                "egg": 46,
                 "image": "ghcr.io/pterodactyl/yolks:java_18",
                 "skip_scripts": False
             }
         },
         {
-            "id":17,
+            "id":2,
             "body":{
-                "startup": "java -Xmx{{SERVER_MEMORY}}M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs/ -Daikars.new.flags=true --add-modules=jdk.incubator.vector -Dterminal.jline=false -Dterminal.ansi=true $( [[  ! -f unix_args.txt ]] && printf %s \"-jar {{SERVER_JARFILE}}\" || printf %s \"@unix_args.txt\" )",
+                "startup": "java  -Xmx$(({{SERVER_MEMORY}}-1024))M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs/ -Daikars.new.flags=true --add-modules=jdk.incubator.vector -jar {{SERVER_JARFILE}}",
                 "environment": {
+                    "PROJECT":"fabric",
+                    "SERVER_JARFILE": "fabric.jar",
+                    "MINECRAFT_VERSION": "latest",
+                    "BUILD_NUMBER":"latest",
+                    "DL_PATH": null,
+                    "FABRIC_VERSION":"latest",
+                    "LOADER_VERSION":"latest",
+                    "BUILD_TYPE":"recommended",
+                    "FORGE_VERSION":null,
+                    "BUNGEE_VERSION":"latest"
+                },
+                "egg": 46,
+                "image": "ghcr.io/pterodactyl/yolks:java_18",
+                "skip_scripts": False
+            }
+        },
+        {
+            "id":3,
+            "body":{
+                "startup": "java -Xmx$(({{SERVER_MEMORY}}-1024))M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs/ -Daikars.new.flags=true --add-modules=jdk.incubator.vector -Dterminal.jline=false -Dterminal.ansi=true $( [[  ! -f unix_args.txt ]] && printf %s \"-jar {{SERVER_JARFILE}}\" || printf %s \"@unix_args.txt\" )",
+                "environment": {
+                    "PROJECT":"forge",
                     "SERVER_JARFILE": "forge.jar",
-                    "MC_VERSION": "latest",
-                    "BUILD_TYPE":"recommended"
+                    "MINECRAFT_VERSION": "latest",
+                    "BUILD_TYPE":"recommended",
+                    "DL_PATH": null,
+                    "FABRIC_VERSION":"latest",
+                    "LOADER_VERSION":"latest",
+                    "BUILD_TYPE":"recommended",
+                    "FORGE_VERSION":null,
+                    "BUNGEE_VERSION":"latest"
                 },
-                "egg": 17,
+                "egg": 46,
                 "image": "ghcr.io/pterodactyl/yolks:java_18",
                 "skip_scripts": False
             }
         },
         {
-            "id":25,
+            "id":4,
             "body":{
-                "startup": "java  -Dterminal.jline=false -Dterminal.ansi=true  -Xmx{{SERVER_MEMORY}}M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs/ -Daikars.new.flags=true --add-modules=jdk.incubator.vector -jar {{SERVER_JARFILE}}",
+                "startup": "java  -Dterminal.jline=false -Dterminal.ansi=true  -Xmx$(({{SERVER_MEMORY}}-1024))M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs/ -Daikars.new.flags=true --add-modules=jdk.incubator.vector -jar {{SERVER_JARFILE}}",
                 "environment": {
+                    "PROJECT":"purpur",
                     "SERVER_JARFILE": "purpur.jar",
                     "MINECRAFT_VERSION": "latest",
-                    "BUILD_NUMBER":"latest"
+                    "BUILD_NUMBER":"latest",
+                    "DL_PATH": null,
+                    "FABRIC_VERSION":"latest",
+                    "LOADER_VERSION":"latest",
+                    "BUILD_TYPE":"recommended",
+                    "FORGE_VERSION":null,
+                    "BUNGEE_VERSION":"latest"
                 },
-                "egg": 25,
+                "egg": 46,
                 "image": "ghcr.io/pterodactyl/yolks:java_18",
                 "skip_scripts": False
             }
         },
         {
-            "id":22,
+            "id":5,
             "body":{
-                "startup": "java -Xmx{{SERVER_MEMORY}}M -XX:MaxRAMPercentage=95.0 -jar {{SERVER_JARFILE}}",
+                "startup": "java -Xmx$(({{SERVER_MEMORY}}-512))M -XX:MaxRAMPercentage=95.0 -jar {{SERVER_JARFILE}}",
                 "environment": {
+                    "PROJECT":"bungeecord",
                     "SERVER_JARFILE": "bungeecord.jar",
-                    "BUNGEE_VERSION": "latest"
+                    "BUNGEE_VERSION": "latest",
+                    "DL_PATH": null,
+                    "FABRIC_VERSION":"latest",
+                    "LOADER_VERSION":"latest",
+                    "BUILD_TYPE":"recommended",
+                    "FORGE_VERSION":null,
+                    "BUNGEE_VERSION":"latest"
                 },
-                "egg": 22,
+                "egg": 46,
                 "image": "ghcr.io/pterodactyl/yolks:java_18",
                 "skip_scripts": False
             }
         },
         {
-            "id":21,
+            "id":6,
             "body":{
-                "startup": "java -Xmx{{SERVER_MEMORY}}M -Dterminal.jline=false -Dterminal.ansi=true -jar {{SERVER_JARFILE}}",
+                "startup": "java -Xmx$(({{SERVER_MEMORY}}-512))M -Dterminal.jline=false -Dterminal.ansi=true -jar {{SERVER_JARFILE}}",
                 "environment": {
+                    "PROJECT":"waterfall",
                     "SERVER_JARFILE": "waterfall.jar",
                     "MINECRAFT_VERSION": "latest",
-                    "BUILD_NUMBER":"latest"
+                    "BUILD_NUMBER":"latest",
+                    "DL_PATH": null,
+                    "FABRIC_VERSION":"latest",
+                    "LOADER_VERSION":"latest",
+                    "BUILD_TYPE":"recommended",
+                    "FORGE_VERSION":null,
+                    "BUNGEE_VERSION":"latest"
                 },
-                "egg": 21,
+                "egg": 46,
                 "image": "ghcr.io/pterodactyl/yolks:java_18",
                 "skip_scripts": False
             }
         },
         {
-            "id":24,
+            "id":7,
             "body":{
-                "startup": "java -Xmx{{SERVER_MEMORY}}M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs/ -Daikars.new.flags=true --add-modules=jdk.incubator.vector -jar {{SERVER_JARFILE}}",
+                "startup": "java -Xmx$(({{SERVER_MEMORY}}-1024))M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs/ -Daikars.new.flags=true --add-modules=jdk.incubator.vector -jar {{SERVER_JARFILE}}",
                 "environment": {
+                    "PROJECT":"folia",
                     "SERVER_JARFILE": "folia.jar",
                     "MINECRAFT_VERSION": "latest",
-                    "BUILD_NUMBER":"latest"
+                    "BUILD_NUMBER":"latest",
+                    "DL_PATH": null,
+                    "FABRIC_VERSION":"latest",
+                    "LOADER_VERSION":"latest",
+                    "BUILD_TYPE":"recommended",
+                    "FORGE_VERSION":null,
+                    "BUNGEE_VERSION":"latest"
                 },
-                "egg": 24,
+                "egg": 46,
                 "image": "ghcr.io/pterodactyl/yolks:java_18",
                 "skip_scripts": False
             }
@@ -110,66 +173,47 @@ server_details=[
         
     ]
 
-headers= {
-            "Authorization": 'Bearer ptla_ebaFM0p5C6kJJruwF2XPbeuTuc9FOUIh2tWSyohA0Qv',
-            "Content-type": 'application/json',
-            "Accept": 'application/json',
-        }
+server_req_args = reqparse.RequestParser()
 
-def verify_user(user_id:int,server_id:int,requested_type:int):
+def download_file(server_full_uuid:str,download_url:str,client_header:str):
+    url_download = f'https://panel.infinity-projects.de/api/client/servers/{server_full_uuid}/files/pull'
+    body={
+        "root": "/",
+        "url": f"{download_url}"
+    }
+    result = requests.post(url=url_download,headers=client_header,json=body)
+
+
+
+def verify_user(user_id:int,server_id:int,requested_type:int,app_header:str):
     url_verify_ownership=f'https://panel.infinity-projects.de/api/application/servers/{server_id}'
-    result = requests.get(url=url_verify_ownership,headers=headers).json()
-    print(f'result from verifying {result["attributes"]["user"]}')
-    if int(result["attributes"]["user"])!=int(user_id):
-        print(f'user {user_id} is not the owner, reporting this incident')
-        return 0
+    result = requests.get(url=url_verify_ownership,headers=app_header).json()
+    if result["user"]!=user_id:
+        return False
     else:
-        details=next((item for item in server_details if int(item['id'])==int(requested_type)))['body']
+        details=next((item for item in server_details if item['id']==requested_type))
         change_type(server_id,json.dumps(details))
+        
 
-def change_type(server_id:int,details):
-    print('details used for changing server: ',details)
+def change_type(server_id:int,details:json,app_header:str):
     url_change_type=f'https://panel.infinity-projects.de/api/application/servers/{server_id}/startup'
-    result = requests.patch(url=url_change_type,headers=headers,data=details).json()
-    print(result)
-    server_uuid = result["attributes"]["identifier"]
-    print(f'server_uuid is : {server_uuid}')
-    reinstall_server(server_id=server_id)
+    result = requests.post(url=url_change_type,headers=app_header,data=details)
 
-def reinstall_server(server_id):
-    print('reinstalling server now\n')
-    url_reinstall_server=f'https://panel.infinity-projects.de/api/application/servers/{server_id}/reinstall'
-    result = requests.post(url=url_reinstall_server,headers=headers)
-    print(result)
-    start_server(server_id)
 
-def start_server(server_id):
-    url_restart_server=f'https://panel.infinity-projects.de/api/application/servers/{server_id}/power'
-    header_start=headers
-    header_start["Authorization"] = "Bearer ptlc_CyPQOKVtwK8VObE2GLRvDs1u95ju9lg3n4VrMp1PNKc"
-    print('processing server startup')
-    result = requests.post(url=url_restart_server,headers=header_start)
-    print(result.json())
+class MinecraftChangeType(Resource):
+    def get(self):
+        return "Hello damdam"
+    def post(self,server_id,owner_id):
+        return f"Server ID : {server_id}, Server Owner: {owner_id}"
     
-
-async def start_type_change(user_id:int,server_id:int):
-    async with httpx.AsyncClient() as session:
-        tasks=[]
-        tasks.append(asyncio.create_task(verify_user(user_id=user_id,server_id=server_id)))
-        result = await asyncio.gather(*tasks)
-    return result
-
-# @app.route('/server_details', methods=['GET'])
-# async def change_type():
-#   test = await verify_user(12,114)
-#   if not test:
-#     return "no!"
-#   return json.dumps(server_details)
-
-
-
-
-
-if __name__ == '__main__':
-    verify_user(sys.argv[1],sys.argv[2],sys.argv[3])
-    # app.run(host='0.0.0.0',port=46752,debug=True)
+        
+class server:
+    app = Flask("PanelAPI")
+    api = Api(app)
+    def __init__(self):
+        pass
+    def start(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('title',required=True)
+        server.api.add_resource(MinecraftChangeType, '/mc-type-change')
+        server.app.run(host='0.0.0.0',port=46752,debug=True)
