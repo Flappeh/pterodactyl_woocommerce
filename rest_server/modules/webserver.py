f2dd52a9c707c65fb2f244ac0924101d5362ba0d
flappeh <bonar_david@rocketmail.com>
2023-06-13 17:05:58 +0000

    add arguments to post request.

--- rest_server/modules/webserver.py
+++ rest_server/modules/webserver.py
@@ -173,20 +173,25 @@ server_details=[
         
     ]
 
+change_type_args = reqparse.RequestParser()
+change_type_args.add_argument("server_id", type=int, help="External ID of server", required=True)
+change_type_args.add_argument("owner_id", type=int, help="External ID of server owner", required=True)
+change_type_args.add_argument("request_type", type=int, help="Type of minecraft server requested", required=True)
 
-def download_file(server_full_uuid:str,download_url:str,client_header:str):
+
+def download_file(server_full_uuid:str,download_url:str):
     url_download = f'https://panel.infinity-projects.de/api/client/servers/{server_full_uuid}/files/pull'
     body={
         "root": "/",
         "url": f"{download_url}"
     }
-    result = requests.post(url=url_download,headers=client_header,json=body)
+    result = requests.post(url=url_download,headers=header_client,json=body)
 
 
 
-def verify_user(user_id:int,server_id:int,requested_type:int,app_header:str):
+def verify_user(user_id:int,server_id:int,requested_type:int):
     url_verify_ownership=f'https://panel.infinity-projects.de/api/application/servers/{server_id}'
-    result = requests.get(url=url_verify_ownership,headers=app_header).json()
+    result = requests.get(url=url_verify_ownership,headers=header_app).json()
     if result["user"]!=user_id:
         return False
     else:
@@ -199,14 +204,19 @@ def change_type(server_id:int,details:json,app_header:str):
     result = requests.post(url=url_change_type,headers=app_header,data=details)
 
 
-
 class MinecraftChangeType(Resource):
     def get(self):
         return "Hello damdam"
-    def post(self,server_id,owner_id):
-        return f"Server ID : {server_id}, Server Owner: {owner_id}"
+    def post(self,server_id,owner_id,requested_type):
+        args = change_type_args.parse_args()
+        
+        if verify_user==False:
+            abort(404, message="User is not the owner of server...")
+        else:
+            pass
+        return f"Server ID : {server_id}, Server Owner: {owner_id}, requested Type: {requested_type}"
     
-
+        
 class server:
     app = Flask("PanelAPI")
     api = Api(app)
