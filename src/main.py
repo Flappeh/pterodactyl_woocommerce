a85ff7ecd290cc172f10a29849fa7e2672f2ac0f
flappeh <bonar_david@rocketmail.com>
2025-02-27 00:11:15 +0700

    Add pydactyl

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from modules.wcom import WooConn
+from modules.panel import PanelServer
 
 
 
 
 if __name__ ==  "__main__":
-    conn = WooConn()
-    conn.list_orders()
+    conn_panel = PanelServer()
+    conn_panel.get_all_servers()
     
---
+++ src/modules/panel.py
@@ -0,0 +1,11 @@
+from pydactyl.api.servers import Servers
+from config import PANEL_URL, PANEL_API
+
+
+class PanelServer(Servers):
+    def __init__(self, session=None):
+        super().__init__(PANEL_URL, PANEL_API, session)
+    
+    def get_all_servers(self):
+        data = self.list_servers()
+        print(data)
